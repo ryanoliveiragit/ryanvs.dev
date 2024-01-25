902f151ce1de10414982f3c1e6c9369742481657
Ryan Oliveira Brito <ryanoliveirabrito178@gmail.com>
2024-01-25 15:58:04 -0300

    plausibleProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Header } from "@/components/header";
 import { Layout } from "@/components/layout";
 import { ThemeProvider } from "@/components/theme-provider";
 import { GoogleAnalytics } from "@next/third-parties/google";
-
+import PlausibleProvider from "next-plausible";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -25,26 +25,23 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <GoogleAnalytics gaId="G-M7QWT8CMKQ" />
-      <script
-        defer
-        data-domain="ryanvs.dev"
-        src="https://plausible.io/js/script.js"
-      ></script>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Header />
+      <PlausibleProvider domain="ryanvs.dev">
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
 
-          <Layout>
-            <span className="rounded-full bg-gradient-to-r dark:from-rose-700 dark:via-pink-700 dark:to-purple-700 dark:opacity-60 from-blue-300 via-cyan-300 to-green-300 opacity-70 -z-50 aspect-square w-full max-w-screen-lg blur-3xl filter bottom-[calc(100%-200px)] dark:bottom-[calc(100%-180px)] fixed"></span>
-            {children}
-          </Layout>
-        </ThemeProvider>
-      </body>
+            <Layout>
+              <span className="rounded-full bg-gradient-to-r dark:from-rose-700 dark:via-pink-700 dark:to-purple-700 dark:opacity-60 from-blue-300 via-cyan-300 to-green-300 opacity-70 -z-50 aspect-square w-full max-w-screen-lg blur-3xl filter bottom-[calc(100%-200px)] dark:bottom-[calc(100%-180px)] fixed"></span>
+              {children}
+            </Layout>
+          </ThemeProvider>
+        </body>
+      </PlausibleProvider>
     </html>
   );
 }
